dd64db380259bb15d5d4104e636f8aff55b0e66e
Hidayat <hidayetmmdff@gmail.com>
2022-01-19 14:44:28 +0400

    dark mode fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>();
   useEffect(() => {
     setIsDarkMode(themeMode === "dark");
-  }, []);
+  }, [themeMode]);
 
   const themeChangeHandler = (mode: ThemeMode) => {
     dispatch(uiActions.toggle(mode));
